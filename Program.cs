884591a4ea0540976e5650c8a61103cd6c9e0ce9
black <black@example.com>
2026-07-04 19:43:34 +0500

    Add greetBlack method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("hello everyone");
+
+void GreetWhite()
+{
+    Console.WriteLine("hello white");
+}
+
+void GreetBlack()
+{
+    Console.WriteLine("hello black");
+}
